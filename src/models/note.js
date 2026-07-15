5b2b1b7d0dfaedc9b152718b632f2aa2a8da09ad
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-07-15 17:44:55 +0200

    Fixed note.js enum TAGS

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -15,18 +16,7 @@ const noteSchema = new Schema(
     tag: {
       type: String,
       default: 'Todo',
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo'
-      ],
+      enum: TAGS,
     },
   },
   { timestamps: true },
